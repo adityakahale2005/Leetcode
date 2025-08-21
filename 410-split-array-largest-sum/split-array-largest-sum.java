797e655065b38688d3fbd31fdcdf7acfc1bcaf58
adityakahale2005 <adikahale@gmail.com>
2025-08-21 21:41:00 +0530

    Time: 0 ms (100.00%) | Memory: 41.2 MB (83.72%) - LeetSync

---
+++ 410-split-array-largest-sum/split-array-largest-sum.java
@@ -0,0 +1,42 @@
+class Solution {
+    public static int countPartitions(int[] a, int maxSum) {
+        int n = a.length; // size of array
+        int partitions = 1;
+        long subarraySum = 0;
+        for (int i = 0; i < n; i++) {
+            if (subarraySum + a[i] <= maxSum) {
+                // insert element to current subarray
+                subarraySum += a[i];
+            } else {
+                // insert element to next subarray
+                partitions++;
+                subarraySum = a[i];
+            }
+        }
+        return partitions;
+    }
+
+    public int splitArray(int[] nums, int k) {
+        int low = nums[0];
+        int high = 0;
+
+        // find maximum element (for low) and total sum (for high)
+        for (int i = 0; i < nums.length; i++) {
+            low = Math.max(low, nums[i]);
+            high += nums[i];
+        }
+
+        // binary search on answer
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            int partitions = countPartitions(nums, mid);
+
+            if (partitions > k) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return low;
+    }
+}
