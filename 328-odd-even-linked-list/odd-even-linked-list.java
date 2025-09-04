f0be15cc1c68727f3885f424c8dab6acd708dff3
adityakahale2005 <adikahale@gmail.com>
2025-09-04 16:12:09 +0530

    Time: 0 ms (100.00%) | Memory: 44.6 MB (64.92%) - LeetSync

--- 328-odd-even-linked-list/odd-even-linked-list.java
+++ 328-odd-even-linked-list/odd-even-linked-list.java
@@ -8,21 +8,32 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+//  we have to sort odd and even "INDEX" not Nos
 class Solution {
     public ListNode oddEvenList(ListNode head) {
         if(head == null){
             return null;
         }
 
-        ListNode odd = head , even = head.next;
-        ListNode temp = odd , curr = even;
-        while(curr != null && curr.next != null && temp != null && temp.next != null){
-            temp.next = curr.next;
-            temp = curr.next;
-            curr.next = temp.next;
-            curr = temp.next;
+        // ListNode odd = head , even = head.next;
+        // ListNode temp = odd , curr = even;
+        // while(curr != null && curr.next != null && temp != null && temp.next != null){
+        //     temp.next = curr.next;
+        //     temp = curr.next;
+        //     curr.next = temp.next;
+        //     curr = temp.next;
+        // }
+        // temp.next = even;
+        // return odd;
+
+        ListNode odd = head , even=head.next , even_head = head.next;
+        while(even!= null && even.next != null){
+            odd.next = odd.next.next;
+            even.next = even.next.next;
+            odd = odd.next;
+            even = even.next;
         }
-        temp.next = even;
-        return odd;
+        odd.next = even_head;
+        return head;
     }
 }
