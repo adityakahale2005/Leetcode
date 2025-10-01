07874114a44d1a13c96ac8e3479d7f041b199eea
adityakahale2005 <adikahale@gmail.com>
2025-10-01 22:34:26 +0530

    Time: 0 ms (100.00%) | Memory: 41.3 MB (83.28%) - LeetSync

--- 225-implement-stack-using-queues/implement-stack-using-queues.java
+++ 225-implement-stack-using-queues/implement-stack-using-queues.java
@@ -8,11 +8,11 @@ class MyStack {
     public void push(int x) {
         q.add(x);
         for(int i=0;i<q.size()-1;i++)
-        q.add(q.poll());
+        q.add(q.remove());
     }
     
     public int pop() {
-        return q.poll();
+        return q.remove();
     }
     
     public int top() {
