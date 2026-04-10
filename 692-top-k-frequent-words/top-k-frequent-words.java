32b46873b751f8e0784bc0f6db4e9d3e181a5a64
adityakahale2005 <adikahale@gmail.com>
2026-04-10 09:33:33 +0530

    Time: 6 ms (99.55%) | Memory: 46.3 MB (88.72%) - LeetSync

--- 692-top-k-frequent-words/top-k-frequent-words.java
+++ 692-top-k-frequent-words/top-k-frequent-words.java
@@ -1,33 +1,43 @@
+import java.util.*;
+
 class Solution {
     public List<String> topKFrequent(String[] words, int k) {
-        
-        HashMap<String , Integer> map = new HashMap<>();
-        
-         // Step 1: Frequency map
-        for(String word : words){
-            map.put(word , map.getOrDefault(word,0)+1);
-        }
-        
-         // Step 2: Max Heap Since we want sorted output
-        PriorityQueue<String> pq = new PriorityQueue<>(
-            (a,b) -> {
-            if(map.get(a).equals(map.get(b))){
-                    return a.compareTo(b); // lexicographically smaller first
-                }
-            return map.get(b) - map.get(a);
-            }
-        );
 
-       // Step 3: Add all words
-       for(String key : map.keySet()){
-            pq.add(key);
+        HashMap<String, Integer> map = new HashMap<>();
+
+        // Step 1: Frequency map
+        for(String word : words){
+            map.put(word, map.getOrDefault(word, 0) + 1);
         }
-        
-        // Step 4: Extract top k
+
+        // Step 2: Min Heap (size k)
+        PriorityQueue<String> pq = new PriorityQueue<>(new Comparator<String>() {
+            @Override
+            public int compare(String w1, String w2) {
+                int f1 = map.get(w1);
+                int f2 = map.get(w2);
+
+                if(f1 == f2) return w2.compareTo(w1); // reverse lex
+                return f1 - f2; // smaller freq first
+            }
+        });
+
+        // Step 3: Maintain only top k
+        for(String key : map.keySet()){
+            pq.add(key);
+            if(pq.size() > k){
+                pq.poll(); // remove worst
+            }
+        }
+
+        // Step 4: Extract result
         List<String> result = new ArrayList<>();
-        for(int i = 0; i < k; i++){
+        while(!pq.isEmpty()){
             result.add(pq.poll());
         }
+
+        // Step 5: Reverse to get correct order
+        Collections.reverse(result);
 
         return result;
     }
